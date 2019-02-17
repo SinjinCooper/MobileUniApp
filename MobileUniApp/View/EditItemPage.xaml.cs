4013766ecce2970133f4efcec940fe3795c2717e
Sinjin Cooper <coopersinjin@gmail.com>
2019-02-16 18:47:07 -0800

    add build type methods

--- MobileUniApp/View/EditItemPage.xaml.cs
+++ MobileUniApp/View/EditItemPage.xaml.cs
@@ -10,20 +10,36 @@ namespace MobileUniApp.View
         public EditItemPage(Term term)
         {
             InitializeComponent();
+            pageTitle.Text = "Edit Term";
+            typeId.Text = "Term Id: " + term.TermId;
             title.Text = term.Title;
             statusPicker.SelectedItem = term.Status;
+            startDatePicker.Date = term.StartDate;
+            endDatePicker.Date = term.EndDate;
         }
 
         public EditItemPage(Course course)
         {
             InitializeComponent();
+            pageTitle.Text = "Edit Course";
+            typeId.Text = "Course Id: " + course.CourseId;
             title.Text = course.Title;
+            statusPicker.SelectedItem = course.Status;
+            startDatePicker.Date = course.StartDate;
+            endDatePicker.Date = course.EndDate;
         }
 
         public EditItemPage(Assessment assessment)
         {
             InitializeComponent();
+            pageTitle.Text = "Edit Assessment";
+            typeId.Text = "Assessment Id: " + assessment.AssessmentId;
             title.Text = assessment.Title;
+            statusPicker.IsVisible = false;
+            typePicker.IsVisible = true;
+            typePicker.SelectedItem = assessment.AssessmentType;
+            startDatePicker.Date = assessment.StartDate;
+            endDatePicker.Date = assessment.EndDate;
         }
 
         // Instructor editing page will look different
