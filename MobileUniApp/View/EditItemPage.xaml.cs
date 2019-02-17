25e1f5bb6752df8f987689539ca6003675eafdcd
Sinjin Cooper <coopersinjin@gmail.com>
2019-02-16 18:36:04 -0800

    add EditPage

---
+++ MobileUniApp/View/EditItemPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MobileUniApp.View
+{
+    public partial class EditItemPage : ContentPage
+    {
+        public EditItemPage(Term term)
+        {
+            InitializeComponent();
+            title.Text = term.Title;
+            statusPicker.SelectedItem = term.Status;
+        }
+
+        public EditItemPage(Course course)
+        {
+            InitializeComponent();
+            title.Text = course.Title;
+        }
+
+        public EditItemPage(Assessment assessment)
+        {
+            InitializeComponent();
+            title.Text = assessment.Title;
+        }
+
+        // Instructor editing page will look different
+        public EditItemPage(Instructor instructor)
+        {
+            InitializeComponent();
+        }
+
+
+
+        public void SaveButtonClicked(object sender, EventArgs e)
+        {
+
+            ClosePage();
+        }
+
+        public void CancelButtonClicked(object sender, EventArgs e)
+        {
+            ClosePage();
+        }
+
+        async void ClosePage()
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
